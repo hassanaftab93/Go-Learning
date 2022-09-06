2e07ff62e017c81a6e16f60c2ae87917a3fdbf7d
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-09-07 00:09:47 +0500

    Constants added

---
+++ Constants/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const a int = 1
+	const b float32 = 2.1
+	const c bool = true
+	const d string = "\"Constant\""
+
+	fmt.Printf("\na: %v\nb: %v\nc: %v\nd: %v\n", a, b, c, d)
+}
