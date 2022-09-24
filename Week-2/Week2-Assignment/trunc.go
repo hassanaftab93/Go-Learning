c087785aa4e126f5486e85f90823e5181b52f998
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-09-25 01:35:07 +0500

    Assignment2

--- Week-2/Week2-Assignment/trunc.go
+++ Week-2/Week2-Assignment/trunc.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bufio" // for Input with spaces
 	"fmt"
 	"math"
+	"os"      // for bufio
+	"strings" // for strings
 )
 
+// Assignment 1
 // Write a program which prompts the user to enter a floating point number
 // and prints the integer which is a truncated version of the floating point number that was entered.
 // Truncation is the process of removing the digits to the right of the decimal place.
 
-func main() {
+func Assignment1() {
 
 	var userNum float32
 	var err error
@@ -22,4 +26,47 @@ func main() {
 	} else {
 		fmt.Println("\nError: ", err)
 	}
+}
+
+// Assignment 2
+/*
+
+Write a program which prompts the user to enter a string.
+The program searches through the entered string for the characters ‘i’, ‘a’, and ‘n’.
+The program should print “Found!” if the entered string starts with the character ‘i’,
+ends with the character ‘n’, and contains the character ‘a’.
+The program should print “Not Found!” otherwise.
+The program should not be case-sensitive,
+so it does not matter if the characters are upper-case or lower-case.
+
+Examples: The program should print “Found!” for the following example entered strings,
+“ian”, “Ian”, “iuiygaygn”, “I d skd a efju N”.
+The program should print “Not Found!” for the following strings, “ihhhhhn”, “ina”, “xian”.
+
+*/
+
+func Assignment2() {
+
+	var temp string
+
+	fmt.Print("\nEnter string: ")
+	userInput := bufio.NewScanner(os.Stdin)
+	userInput.Scan()
+
+	line := userInput.Text()
+
+	temp = strings.ToLower(line)
+
+	if strings.HasPrefix(temp, "i") && strings.Contains(temp, "a") && strings.HasSuffix(temp, "n") {
+		fmt.Println("\nFound!")
+	} else {
+		fmt.Println("\nNot Found!")
+	}
+}
+
+func main() {
+
+	//Assignment1()
+	Assignment2()
+
 }
