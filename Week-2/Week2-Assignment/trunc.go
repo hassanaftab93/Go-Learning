c227925f29a4c6bc19a6b14ec97d5b44a26b8bc2
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-09-25 00:45:42 +0500

    Week-2 Assignment

--- Week-2/Week2-Assignment/trunc.go
+++ Week-2/Week2-Assignment/trunc.go
@@ -5,6 +5,10 @@ import (
 	"math"
 )
 
+// Write a program which prompts the user to enter a floating point number
+// and prints the integer which is a truncated version of the floating point number that was entered.
+// Truncation is the process of removing the digits to the right of the decimal place.
+
 func main() {
 
 	var userNum float32
