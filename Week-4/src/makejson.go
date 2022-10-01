8a3a47de4218c4f70f230180eed105bd7204cdc4
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-10-01 19:17:08 +0500

    Assignment 1 in Progress

--- Week-4/src/makejson.go
+++ Week-4/src/makejson.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 /*
 Write a program which prompts the user to first enter a name, and then enter an address.
@@ -14,6 +18,24 @@ and then your program should print the JSON object.
 
 */
 
+func getName() string {
+	fmt.Println("\nEnter Name: ")
+	userinput := bufio.NewScanner(os.Stdin)
+	userinput.Scan()
+	output := userinput.Text()
+	return output
+}
+
+func getAddr() string {
+	fmt.Println("\nEnter Address: ")
+	userinput := bufio.NewScanner(os.Stdin)
+	userinput.Scan()
+	output := userinput.Text()
+	return output
+}
+
 func main() {
 	fmt.Println("\nWeek 4 - Assignment 1")
+	getName()
+	getAddr()
 }
