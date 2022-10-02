7d35164297867f4b410987f4b0abf77645c929da
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-10-02 17:13:25 +0500

    Week 2 Started

---
+++ Module-2/Week-2/go.mod
@@ -0,0 +1,3 @@
+module github.com/hassanaftab93/go-learning/Module-2/Week-2
+
+go 1.19
---
+++ Module-2/Week-2/src/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+	Notes
+
+- Module 2 - Week 2
+*/
+func main() {
+	fmt.Println("\nModule-2 Week-2")
+}
