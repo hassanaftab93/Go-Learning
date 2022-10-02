b1a43e5d88b00dbe06eab293ac48481ee93277ee
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-10-02 15:25:47 +0500

    Added Presentations

--- Module-2/Week-1/src/main.go
+++ Module-2/Week-1/src/main.go
@@ -4,6 +4,44 @@ import (
 	"fmt"
 )
 
+//Functions
+
+//Parameters and return Values
+
+//Call by Value or Reference
+
+//Passing arrays and slices
+
+//Well written functions
+
+/*
+1. Understandability is Important
+	1.1. If you are requested to find a feature, if found easily, then Code is understandable
+	1.2. Where data is used/found, Traceability should be easy
+	1.3. Maybe data is bad/wrong in a file being read, Issue should be traceable
+
+2. Debugging Principles
+	2.1. There are 2 Possibilities
+		2.1.1. Function is written incorrectly (Logic needs to be revised, try Dry running)
+		2.1.2. Maybe the Data being passed into the function is wrong. (Try tracing back, Console logs can help)
+
+3. Supporting debugging
+	3.1. Functions need to be understandable
+	3.2. Is the function performing its purpose correctly?
+	3.3. Data needs to be traceable
+		3.3.1. Global Variable complicate this
+		3.3.2. Try to code in a way such that all the data is going Func -> Func
+*/
+
+//Guidelines for functions
+
+/*
+1. Function Naming
+	- Understandable names
+	- Parameter names
+	- Functional Cohesion is important | one Function should perfrom one Operation.
+*/
+
 func main() {
 	fmt.Println("\nModule-2 Week-1")
 }
