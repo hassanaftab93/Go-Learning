e4f9405099fa85b928ba0a6c316a4fb35a93ef7d
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-10-02 17:19:06 +0500

    Notes

--- Module-2/Week-2/src/main.go
+++ Module-2/Week-2/src/main.go
@@ -8,6 +8,11 @@ import (
 	Notes
 
 - Module 2 - Week 2
+
+ 1. First Class Values
+    - variables as functions
+ 2. Returning Functions
+ 3. Variadic and Deferred
 */
 func main() {
 	fmt.Println("\nModule-2 Week-2")
